4bb8e7716a25a5158c8ec6d8ba5ce2ddb22f9d39
Adam Weber <adamweber01@gmail.com>
2016-04-22 14:39:51 -0500

    updated build

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack');
 
 module.exports = {
     entry: [
-        'webpack-dev-server/client?http://0.0.0.0:8080',
-        'webpack/hot/only-dev-server',
+        // 'webpack-dev-server/client?http://0.0.0.0:8080',
+        // 'webpack/hot/only-dev-server',
         './example/app.js'
     ],
     output: {
